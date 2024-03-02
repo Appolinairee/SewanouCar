fdb99afcfdfaf28de855cdd75009ba946576aa66
Appolinairee <adandappolinaire229@gmail.com>
2024-03-02 17:45:04 +0100

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '', 
+  base: '/',  // ou base: ''
   plugins: [react()],
   build: {
     outDir: 'dist',
